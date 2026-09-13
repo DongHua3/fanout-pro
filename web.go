0a1acb23943c2d52ae392ac57fda369af4d9b2a3
DongHua3 <1420598057@qq.com>
2026-09-13 14:51:48 +0800

    fix: 添加禁止缓存头确保网页更新即时生效

--- web.go
+++ web.go
@@ -8,6 +8,9 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
 	_, _ = w.Write([]byte(indexHTML))
 }
 
